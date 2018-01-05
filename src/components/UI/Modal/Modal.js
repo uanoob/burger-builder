08848debdff06b53f852ace627c7f64799cf4268
uanoob <usnet@ukr.net>
2018-01-05 16:16:09 +0200

    Handling Errors

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -5,15 +5,17 @@ import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
-
     shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
+        return (
+            nextProps.show !== this.props.show ||
+            nextProps.children !== this.props.children
+        );
     }
 
-    componentWillUpdate () {
+    componentWillUpdate() {
         console.log('[Modal] WillUpdate');
     }
-    
+
     render() {
         return (
             <Aux>
