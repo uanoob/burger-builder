9d3d4d725d6df25d4079a4871e51c1ab282c34e9
uanoob <usnet@ukr.net>
2018-01-09 12:46:23 +0200

    Passing Ingredients Via Query Params, Navigating To The Contact Data Component

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,6 +1,10 @@
+/* eslint-disable */
+
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
@@ -10,11 +14,21 @@ class Checkout extends Component {
       cheese: 1,
       bacon: 1
     }
+  };
+
+  componentDidMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    for (let param of query.entries()) {
+      // ['salad', '1']
+      ingredients[param[0]] = +param[1];
+    }
+    this.setState({ ingredients: ingredients });
   }
 
   checkoutCancelledHandler = () => {
     this.props.history.goBack();
-  }
+  };
 
   checkoutContinuedHandler = () => {
     this.props.history.replace('/checkout/contact-data');
@@ -27,6 +41,10 @@ class Checkout extends Component {
           ingredients={this.state.ingredients}
           checoutCancelled={this.checkoutCancelledHandler}
           checkoutContinued={this.checkoutContinuedHandler}
+        />
+        <Route
+          path={this.props.match.path + '/contact-data'}
+          component={ContactData}
         />
       </div>
     );
