47645fe2ecf39d81f5440e050305648a185fa8b9
uanoob <usnet@ukr.net>
2018-01-17 15:01:00 +0200

    Adding Actions, Getting Token From Backend

---
+++ src/store/actions/auth.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESS,
+    authData: authData
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error: error
+  };
+};
+
+export const auth = (email, password) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email: email,
+      password: password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAsRep1fMID9XN6ETrTzg1n6Q0aBL4csOc',
+        authData
+      )
+      .then(response => {
+        console.log(response);
+        dispatch(authSuccess(response.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
